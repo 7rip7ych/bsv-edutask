d7af796e8943ee0fdff1168d911802ba5946c5f0
Ida Alin <dev.7rip7ych@gmail.com>
2026-05-17 18:57:53 +0200

    Separate actions

--- frontend/cypress/e2e/manipulate.cy.js
+++ frontend/cypress/e2e/manipulate.cy.js
@@ -1,9 +1,7 @@
 // const { beforeEach, it, describe, before, after, cy } = require("node:test")
 
-
 describe('Manipulating the todolist associated to a task', () => {
     let uid
-    let name
     let email
     let taskid
     let todotext
@@ -22,11 +20,11 @@ describe('Manipulating the todolist associated to a task', () => {
                     headers: { 'Cache-Control': 'no-cache' }
                 }).then((response) => {
                     uid = response.body._id.$oid
-                    name = user.firstName + ' ' + user.lastName
                     email = user.email
                 })
             })
             .then(function() {
+                // create task
                 cy.fixture('task.json')
                     .then((task) => {
                         task.userid = uid
@@ -49,27 +47,6 @@ describe('Manipulating the todolist associated to a task', () => {
             })
     })
     beforeEach(function () {
-            // cy.fixture('task.json')
-            //     .then((task) => {
-            //         task.userid = uid
-            //         todotext = task.todos
-            //         cy.request({
-            //             method: 'POST',
-            //             url: 'http://localhost:5000/tasks/create',
-            //             form: true,
-            //             body: task
-            //         }).then((response) => {
-            //             taskid = Object.values(response.body[0]._id)[0]
-            //             todoid = response.body[0].todos[0]._id.$oid
-            //             todos = response.body[0].todos
-            //             cy.log(taskid)
-            //             cy.log(todoid)
-            //             cy.log(JSON.stringify(todos))
-
-                        
-            //         })
-            //     })
-
         cy.visit('http://localhost:3000')
         cy.contains('div', 'Email Address')
             .find('input[type=text]')
@@ -80,14 +57,37 @@ describe('Manipulating the todolist associated to a task', () => {
             .click()
     })
 
-    describe('Adding a todo', () => {
-        it('Adding a todo with empty description', () => {
+    describe('Entering a description', () => {
+        it('description is empty', () => {
             cy.get('.inline-form')
                 .find('input[type=text]')
                 .clear()
             
             cy.get('input[value="Add"]')
                 .should('be.disabled')
+        })
+
+        it('Description is not empty', () => {
+            cy.get('.inline-form')
+                .find('input[type=text]')
+                .type('test description')
+
+            cy.get('input[value="Add"]')
+                .should('not.be.disabled')
+        })
+
+        afterEach(function() {
+            cy.get('.inline-form')
+                .find('input[type=text]')
+                .clear()
+        })
+    })
+
+    describe('Adding a todo', () => {
+        it('Adding a todo with empty description', () => {
+            cy.get('.inline-form')
+                .find('input[type=text]')
+                .clear()
 
             cy.get('input[value="Add"]')
                 .click()
@@ -104,8 +104,6 @@ describe('Manipulating the todolist associated to a task', () => {
                 .type('test description')
 
             cy.get('input[value="Add"]')
-                .should('not.be.disabled')
-            cy.get('input[value="Add"]')
                 .click()
 
             cy.get('ul.todo-list')
@@ -120,34 +118,6 @@ describe('Manipulating the todolist associated to a task', () => {
                 method: 'DELETE',
                 url: `http://localhost:5000/tasks/byid/${taskid}`,
             })
-        //     // .then(function() {
-        //     //     cy.fixture('task.json')
-        //     //         .then((task) => {
-        //     //             task.userid = uid
-        //     //             todotext = task.todos
-        //     //             cy.request({
-        //     //                 method: 'POST',
-        //     //                 url: 'http://localhost:5000/tasks/create',
-        //     //                 form: true,
-        //     //                 body: task
-        //     //             }).then((response) => {
-        //     //                 taskid = Object.values(response.body[0]._id)[0]
-        //     //                 todoid = response.body[0].todos[0]._id.$oid
-        //     //                 todos = response.body[0].todos
-        //     //                 cy.log(taskid)
-        //     //                 cy.log(todoid)
-        //     //                 cy.log(JSON.stringify(todos))
-        //     //             })
-        //     //         })
-        //     // })
-            // cy.request({
-            //     method: 'PUT',
-            //     url: `http://localhost:5000/tasks/byid/${taskid}`,
-            //     form: true,
-            //     body: {'data': `{'$set': {'todos': ${JSON.stringify(todos)}}`}
-            // }).then((response) => {
-            //     cy.log(response.body)
-            // })
             cy.fixture('task.json')
                     .then((task) => {
                         task.userid = uid
@@ -188,8 +158,6 @@ describe('Manipulating the todolist associated to a task', () => {
                     .find('.checked')
                     .should('exist')
             })
-
-            
         })
 
         it('todo is done', () => {
@@ -210,15 +178,15 @@ describe('Manipulating the todolist associated to a task', () => {
             })
         })
 
-        // afterEach(function () {
-        //     const data = {'data': `{'$set': {'done': false}}`}
-        //     cy.request({
-        //         method: 'PUT',
-        //         url: `http://localhost:5000/todos/byid/${todoid}`,
-        //         form: true,
-        //         body: data
-        //     })
-        // })
+        after(function () {
+            const data = {'data': `{'$set': {'done': false}}`}
+            cy.request({
+                method: 'PUT',
+                url: `http://localhost:5000/todos/byid/${todoid}`,
+                form: true,
+                body: data
+            })
+        })
     })
 
 
@@ -228,18 +196,13 @@ describe('Manipulating the todolist associated to a task', () => {
             cy.contains('li', todotext)
                 .find('.remover')
                 .click()
-            
+
             cy.contains('li', todotext)
                 .should('not.exist')
         })
 
     })
 
-
-
-
-
-    
 
     after(function () {
         // clean up by deleting the user from the database
